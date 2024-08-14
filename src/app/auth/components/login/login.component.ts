8387b06371c8d72d5f48f9f7ae5e2c35ec398348
VKTRenokh <viktorenokh@gmail.com>
2024-08-14 14:46:52 +0300

    feat: add @taiga-ui/legacy package

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -6,12 +6,14 @@ import {
 import {
   NonNullableFormBuilder,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
+import { TuiInputModule } from '@taiga-ui/legacy';
 
 @Component({
   selector: 'tra-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, TuiInputModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,5 +21,7 @@ import {
 export class LoginComponent {
   private formBuilder = inject(NonNullableFormBuilder);
 
-  public loginForm = this.formBuilder.group({});
+  public loginForm = this.formBuilder.group({
+    email: this.formBuilder.control('', [Validators.email]),
+  });
 }
