fc734487adff8d7797fe326c886bfd663c789957
VKTRenokh <viktorenokh@gmail.com>
2024-08-14 17:20:54 +0300

    feat: add email input

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -8,12 +8,21 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { TuiError } from '@taiga-ui/core';
 import { TuiInputModule } from '@taiga-ui/legacy';
+import { TuiFieldErrorPipe } from '@taiga-ui/kit';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'tra-login',
   standalone: true,
-  imports: [ReactiveFormsModule, TuiInputModule],
+  imports: [
+    ReactiveFormsModule,
+    TuiInputModule,
+    TuiError,
+    TuiFieldErrorPipe,
+    AsyncPipe,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
