0cf518561dd0e118e5546e9cb458c1e72eccc97c
VKTRenokh <viktorenokh@gmail.com>
2024-08-14 13:07:31 +0300

    feat: add @taiga-ui/styles package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { TuiButton, TuiRoot } from '@taiga-ui/core';
+import { TuiRoot } from '@taiga-ui/core';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'tra-root',
   standalone: true,
-  imports: [RouterOutlet, TuiRoot, TuiButton],
+  imports: [RouterOutlet, TuiRoot],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,14 +1,23 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  inject,
 } from '@angular/core';
+import {
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'tra-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private formBuilder = inject(NonNullableFormBuilder);
+
+  public loginForm = this.formBuilder.group({});
+}
