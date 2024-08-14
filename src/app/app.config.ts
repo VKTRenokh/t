9cee0aff16f7b1fb5f024ee86b7bf5d99568345e
VKTRenokh <viktorenokh@gmail.com>
2024-08-14 10:51:35 +0300

    feat: add ngrx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,15 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-  ],
+    provideStore(),
+    provideEffects()
+],
 };
