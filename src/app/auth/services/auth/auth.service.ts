6b0d35e9da0ebaf11031b798233afecf78910bea
VKTRenokh <viktorenokh@gmail.com>
2024-08-15 11:17:12 +0300

    feat: add profile request

--- src/app/auth/services/auth/auth.service.ts
+++ src/app/auth/services/auth/auth.service.ts
@@ -1,11 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Profile } from '../../models/profile.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private http = inject(HttpClient);
+
+  // TODO: for testing purpose only
+  constructor() {
+    this.signup('vitya@email.com', '12345678');
+  }
 
   public signup(email: string, password: string) {
     return this.http.post('/api/signup', {
@@ -18,6 +24,12 @@ export class AuthService {
     return this.http.post('/api/signin', {
       email,
       password,
+    });
+  }
+
+  public profile(token: string) {
+    return this.http.get<Profile>('/api/profile', {
+      headers: { Authorization: `Bearer ${token}` },
     });
   }
 }
