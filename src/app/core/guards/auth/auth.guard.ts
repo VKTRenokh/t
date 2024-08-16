0e7e260a68cbb0b1d60f9befcc560946320ecb3e
VKTRenokh <viktorenokh@gmail.com>
2024-08-16 22:28:45 +0300

    feat: implement auth guard

--- src/app/core/guards/auth/auth.guard.ts
+++ src/app/core/guards/auth/auth.guard.ts
@@ -1,5 +1,20 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../state/app.state';
+import { selectIsAuthorized } from '../../../state/selectors/auth.selector';
+import { map } from 'rxjs';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+export const authGuard: CanActivateFn = () => {
+  const router = inject(Router);
+
+  return inject<Store<AppState>>(Store)
+    .select(selectIsAuthorized)
+    .pipe(
+      map(isAuthorized =>
+        isAuthorized
+          ? true
+          : router.createUrlTree(['/login']),
+      ),
+    );
 };
