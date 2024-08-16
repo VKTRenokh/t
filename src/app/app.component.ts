9a7381fcef0d4a63f6474365bf093994ba5946f2
ivanFihu <verbipo@gmail.com>
2024-08-16 11:58:05 +0200

    fix: reorder file structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { TuiRoot } from '@taiga-ui/core';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'tra-root',
