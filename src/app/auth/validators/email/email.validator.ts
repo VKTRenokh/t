cc39a0d39911687011fd1721bfb616254c6f6016
VKTRenokh <viktorenokh@gmail.com>
2024-08-16 10:16:24 +0300

    fix: email regexp

--- src/app/auth/validators/email/email.validator.ts
+++ src/app/auth/validators/email/email.validator.ts
@@ -1,7 +1,7 @@
 import { ValidatorFn } from '@angular/forms';
 
 const emailRegex =
-  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{1,}$/;
 
 export const emailValidator: ValidatorFn = control =>
   emailRegex.test(control.value)
