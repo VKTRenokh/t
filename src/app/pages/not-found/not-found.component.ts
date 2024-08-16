e8741fe47902f8e4aed1e424ee1f188e2828d3a7
ivanFihu <verbipo@gmail.com>
2024-08-16 11:47:42 +0200

    feat: 404page

---
+++ src/app/pages/not-found/not-found.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Router } from '@angular/router';
+import { interval, takeWhile, tap, finalize } from 'rxjs';
+
+@Component({
+  selector: 'tra-not-found',
+  standalone: true,
+  imports: [],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss',
+})
+export class NotFoundComponent {
+  private router = inject(Router);
+  counter = 225;
+
+  constructor() {
+    interval(1000)
+      .pipe(
+        takeWhile(() => this.counter > 0),
+        tap(() => {
+          this.counter -= 1;
+        }),
+        finalize(() => this.router.navigate(['/'])),
+        takeUntilDestroyed(),
+      )
+      .subscribe();
+  }
+}
