5280609c614e1f9d5c9175f54fc93ea2dd5febda
VKTRenokh <viktorenokh@gmail.com>
2024-08-16 14:06:45 +0300

    fix: login component test

--- src/app/auth/components/login/login.component.spec.ts
+++ src/app/auth/components/login/login.component.spec.ts
@@ -1,16 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+} from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { AppState } from '../../../state/app.state';
+import { provideMockStore } from '@ngrx/store/testing';
+import { provideRouter } from '@angular/router';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  const initialState: AppState = {
+    auth: { loading: false, isAuthorized: false },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
-    })
-    .compileComponents();
+      imports: [LoginComponent],
+      providers: [
+        provideMockStore({ initialState }),
+        provideRouter([
+          { path: '**', component: LoginComponent },
+        ]),
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
