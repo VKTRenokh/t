f691e8e7106e311eea8e960e294019e277b8b27e
VKTRenokh <viktorenokh@gmail.com>
2024-08-17 13:51:25 +0300

    feat: add taiga-ui date input

--- src/app/search/components/search-page/search-page.component.ts
+++ src/app/search/components/search-page/search-page.component.ts
@@ -1,14 +1,27 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  inject,
 } from '@angular/core';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { TuiDay } from '@taiga-ui/cdk/date-time';
+import { TuiInputDateModule } from '@taiga-ui/legacy';
 
 @Component({
   selector: 'tra-search-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, TuiInputDateModule],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SearchPageComponent {}
+export class SearchPageComponent {
+  private formBuilder = inject(FormBuilder);
+
+  public form = this.formBuilder.group({
+    date: this.formBuilder.control(TuiDay.currentLocal()),
+  });
+}
