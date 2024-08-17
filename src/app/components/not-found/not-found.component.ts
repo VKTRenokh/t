81905c462f8e330d15bbd52207f59e4ed7d9e782
ivanFihu <verbipo@gmail.com>
2024-08-17 09:01:58 +0200

    refactor: simplify 404

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -5,13 +5,8 @@ import {
   inject,
 } from '@angular/core';
 import { Router } from '@angular/router';
-import {
-  TuiBreakpointService,
-  TuiButton,
-  TuiSizeL,
-} from '@taiga-ui/core';
+import { TuiButton } from '@taiga-ui/core';
 import { TuiBlockStatus } from '@taiga-ui/layout';
-import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'tra-not-found',
@@ -22,15 +17,7 @@ import { Observable, map } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NotFoundComponent {
-  protected readonly breakpointService = inject(
-    TuiBreakpointService,
-  );
   private router = inject(Router);
-
-  protected size$: Observable<TuiSizeL> =
-    this.breakpointService.pipe(
-      map(key => (key === 'mobile' ? 'm' : 'l')),
-    );
 
   public goHome() {
     this.router.navigate(['/']);
