e37095833ee1797c5b42eb8379a3e21d31d18cb5
VKTRenokh <viktorenokh@gmail.com>
2024-08-17 21:51:18 +0300

    feat: add from city and to city inputs

--- src/app/search/components/search-page/search-page.component.ts
+++ src/app/search/components/search-page/search-page.component.ts
@@ -6,14 +6,22 @@ import {
 import {
   FormBuilder,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
 import { TuiDay } from '@taiga-ui/cdk/date-time';
-import { TuiInputDateTimeModule } from '@taiga-ui/legacy';
+import {
+  TuiInputDateTimeModule,
+  TuiInputModule,
+} from '@taiga-ui/legacy';
 
 @Component({
   selector: 'tra-search-page',
   standalone: true,
-  imports: [ReactiveFormsModule, TuiInputDateTimeModule],
+  imports: [
+    ReactiveFormsModule,
+    TuiInputDateTimeModule,
+    TuiInputModule,
+  ],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,10 +30,14 @@ export class SearchPageComponent {
   private formBuilder = inject(FormBuilder);
 
   public form = this.formBuilder.group({
-    date: this.formBuilder.control([
-      this.getNextTuiDay(),
-      null,
+    from: this.formBuilder.control('', [
+      Validators.required,
     ]),
+    to: this.formBuilder.control('', [Validators.required]),
+    date: this.formBuilder.control(
+      [this.getNextTuiDay(), null],
+      [Validators.required],
+    ),
   });
 
   public getNextTuiDay() {
