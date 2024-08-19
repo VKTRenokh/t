e5dc0957188534812be337bc06c834d51476f7b8
VKTRenokh <viktorenokh@gmail.com>
2024-08-19 18:11:27 +0300

    feat: implement manager guard

--- src/app/core/guards/manager/manager.guard.ts
+++ src/app/core/guards/manager/manager.guard.ts
@@ -1,3 +1,20 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../state/app.state';
+import { inject } from '@angular/core';
+import { selectRole } from '../../../state/selectors/user.selector';
+import { map } from 'rxjs';
 
-export const managerGuard: CanActivateFn = () => true;
+export const managerGuard: CanActivateFn = () => {
+  const router = inject(Router);
+
+  return inject<Store<AppState>>(Store)
+    .select(selectRole)
+    .pipe(
+      map(role =>
+        role === 'manager'
+          ? true
+          : router.createUrlTree(['/search']),
+      ),
+    );
+};
