6250b5a3ae8e8b5c1e4553360205f62bcf43f77b
VKTRenokh <viktorenokh@gmail.com>
2024-08-19 17:51:48 +0300

    feat: add user init effect

--- src/app/state/effects/user.effect.ts
+++ src/app/state/effects/user.effect.ts
@@ -1,9 +1,36 @@
 import { inject, Injectable } from '@angular/core';
-import { Actions } from '@ngrx/effects';
+import {
+  Actions,
+  createEffect,
+  ofType,
+  ROOT_EFFECTS_INIT,
+} from '@ngrx/effects';
 import { UserService } from '../../core/services/user/user.service';
+import { catchError, exhaustMap, map, of } from 'rxjs';
+import { UserActions } from '../actions/user.action';
 
 @Injectable()
 export class UserEffects {
   private actions = inject(Actions);
   private userService = inject(UserService);
+
+  public initEffect = createEffect(() =>
+    this.actions.pipe(
+      ofType(ROOT_EFFECTS_INIT),
+      exhaustMap(() =>
+        this.userService.profile().pipe(
+          map(user =>
+            UserActions.loadProfileSuccess({ user }),
+          ),
+          catchError(response =>
+            of(
+              UserActions.loadProfileFailure({
+                error: response.error,
+              }),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
