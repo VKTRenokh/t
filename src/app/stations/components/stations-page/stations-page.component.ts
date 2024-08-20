7bb5daed4902c28295733b7897de2312f2e6ca01
VKTRenokh <viktorenokh@gmail.com>
2024-08-20 18:00:30 +0300

    feat: add leaflet map

--- src/app/stations/components/stations-page/stations-page.component.ts
+++ src/app/stations/components/stations-page/stations-page.component.ts
@@ -1,7 +1,11 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
+  ViewChild,
 } from '@angular/core';
+import { map, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'tra-stations-page',
@@ -11,4 +15,22 @@ import {
   styleUrl: './stations-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StationsPageComponent {}
+export class StationsPageComponent
+  implements AfterViewInit
+{
+  @ViewChild('map') public map!: ElementRef;
+
+  public ngAfterViewInit(): void {
+    console.log(this.map);
+    const a = map(this.map.nativeElement);
+    tileLayer(
+      'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      },
+    ).addTo(a);
+
+    a.setView([51.505, -0.09], 13);
+  }
+}
