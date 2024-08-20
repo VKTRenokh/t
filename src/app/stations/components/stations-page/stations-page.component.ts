dce695740b1524c2a5c57d8a904ec8045b2938d2
VKTRenokh <viktorenokh@gmail.com>
2024-08-20 21:22:29 +0300

    feat: move map into separate component

--- src/app/stations/components/stations-page/stations-page.component.ts
+++ src/app/stations/components/stations-page/stations-page.component.ts
@@ -1,36 +1,15 @@
 import {
-  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
-  ElementRef,
-  ViewChild,
 } from '@angular/core';
-import { map, tileLayer } from 'leaflet';
+import { MapComponent } from '../map/map.component';
 
 @Component({
   selector: 'tra-stations-page',
   standalone: true,
-  imports: [],
+  imports: [MapComponent],
   templateUrl: './stations-page.component.html',
   styleUrl: './stations-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StationsPageComponent
-  implements AfterViewInit
-{
-  @ViewChild('map') public map!: ElementRef;
-
-  public ngAfterViewInit(): void {
-    console.log(this.map);
-    const a = map(this.map.nativeElement);
-    tileLayer(
-      'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-      {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      },
-    ).addTo(a);
-
-    a.setView([51.505, -0.09], 13);
-  }
-}
+export class StationsPageComponent {}
