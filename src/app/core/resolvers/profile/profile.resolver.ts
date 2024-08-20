e8274e9e60419ee941ded539c2ac7a87533ee626
VKTRenokh <viktorenokh@gmail.com>
2024-08-20 12:54:07 +0300

    fix: managerGuard

--- src/app/core/resolvers/profile/profile.resolver.ts
+++ src/app/core/resolvers/profile/profile.resolver.ts
@@ -2,10 +2,17 @@ import { inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../state/app.state';
 import { selectRole } from '../../../state/selectors/user.selector';
-import { filter, first } from 'rxjs';
+import { filter, first, tap } from 'rxjs';
 import { isNotNullable } from '../../../shared/utils/is-not-nullables';
+import { ResolveFn } from '@angular/router';
 
-export const profileResolver = () =>
+export const profileResolver: ResolveFn<
+  string | void
+> = () =>
   inject<Store<AppState>>(Store)
     .select(selectRole)
-    .pipe(filter(isNotNullable), first());
+    .pipe(
+      filter(role => isNotNullable(role)),
+      first(),
+      tap(console.log),
+    );
