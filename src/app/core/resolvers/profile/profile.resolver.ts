ac8c8beb0054e9480cba9cdc68039ef0e5636d14
VKTRenokh <viktorenokh@gmail.com>
2024-08-20 14:02:49 +0300

    refactor: remove useless store declaration

--- src/app/core/resolvers/profile/profile.resolver.ts
+++ src/app/core/resolvers/profile/profile.resolver.ts
@@ -5,8 +5,7 @@ import { first } from 'rxjs';
 import { ResolveFn } from '@angular/router';
 import { selectAndFilterRoleAndError } from '../../utils/select-and-filter-role-and-error.util';
 
-export const profileResolver: ResolveFn<unknown> = () => {
-  const store = inject<Store<AppState>>(Store);
-
-  return selectAndFilterRoleAndError(store).pipe(first());
-};
+export const profileResolver: ResolveFn<unknown> = () =>
+  selectAndFilterRoleAndError(
+    inject<Store<AppState>>(Store),
+  ).pipe(first());
