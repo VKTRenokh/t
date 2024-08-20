009adc20130e3d23b5466a7fbce3aa840891f6b5
VKTRenokh <viktorenokh@gmail.com>
2024-08-20 13:39:32 +0300

    fix: manager guard again...

--- src/app/core/guards/manager/manager.guard.ts
+++ src/app/core/guards/manager/manager.guard.ts
@@ -2,24 +2,23 @@ import { CanActivateFn, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../state/app.state';
 import { inject } from '@angular/core';
-import { selectRole } from '../../../state/selectors/user.selector';
-import { map, switchMap, tap } from 'rxjs';
-import { selectError } from '../../../state/selectors/auth.selector';
+import { selectRoleAndError } from '../../../state/selectors/user.selector';
+import { filter, map } from 'rxjs';
+import { isNotNullable } from '../../../shared/utils/is-not-nullables';
 
 export const managerGuard: CanActivateFn = () => {
   const router = inject(Router);
   const store = inject<Store<AppState>>(Store);
 
-  return store.select(selectRole).pipe(
-    switchMap(role =>
-      store.select(selectError).pipe(
-        map(error =>
-          role === 'manager' && !error
-            ? true
-            : router.parseUrl('/search'),
-        ),
-        tap(console.log),
-      ),
+  return store.select(selectRoleAndError).pipe(
+    filter(
+      ({ error, role }) =>
+        isNotNullable(role) || isNotNullable(error),
+    ),
+    map(({ role, error }) =>
+      role === 'manager' && !error
+        ? true
+        : router.createUrlTree(['/']),
     ),
   );
 };
--- src/app/core/resolvers/profile/profile.resolver.ts
+++ src/app/core/resolvers/profile/profile.resolver.ts
@@ -1,18 +1,22 @@
 import { inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../state/app.state';
-import { selectRole } from '../../../state/selectors/user.selector';
-import { filter, first, tap } from 'rxjs';
+import { selectRoleAndError } from '../../../state/selectors/user.selector';
+import { filter, first, map } from 'rxjs';
 import { isNotNullable } from '../../../shared/utils/is-not-nullables';
 import { ResolveFn } from '@angular/router';
 
 export const profileResolver: ResolveFn<
   string | void
-> = () =>
-  inject<Store<AppState>>(Store)
-    .select(selectRole)
-    .pipe(
-      filter(role => isNotNullable(role)),
-      first(),
-      tap(console.log),
-    );
+> = () => {
+  const store = inject<Store<AppState>>(Store);
+
+  return store.select(selectRoleAndError).pipe(
+    filter(
+      ({ error, role }) =>
+        isNotNullable(role) || isNotNullable(error),
+    ),
+    map(() => undefined),
+    first(),
+  );
+};
