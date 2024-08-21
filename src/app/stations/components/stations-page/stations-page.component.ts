cc60322b626147e4fd833cc68e8927596603d671
VKTRenokh <viktorenokh@gmail.com>
2024-08-21 12:48:33 +0300

    feat: make map component custom formControl

--- src/app/stations/components/stations-page/stations-page.component.ts
+++ src/app/stations/components/stations-page/stations-page.component.ts
@@ -1,20 +1,39 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  inject,
 } from '@angular/core';
 import { StationsListComponent } from '../../../stations/components/stations/stations-list.component';
 import { MapComponent } from '../map/map.component';
+import {
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'tra-stations-page',
   standalone: true,
-  imports: [MapComponent, StationsListComponent],
+  imports: [
+    MapComponent,
+    ReactiveFormsModule,
+    AsyncPipe,
+    StationsListComponent,
+  ],
   templateUrl: './stations-page.component.html',
   styleUrl: './stations-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StationsPageComponent {
-  public onMarkerChange(event: unknown) {
-    console.log(event);
+  private formBuilder = inject(NonNullableFormBuilder);
+
+  public form = this.formBuilder.group({
+    latLng: this.formBuilder.control(null),
+  });
+
+  constructor() {
+    this.form.controls.latLng.valueChanges.subscribe(
+      console.log,
+    );
   }
 }
