0230491922689ded9d091a5a599e09e449029135
VKTRenokh <viktorenokh@gmail.com>
2024-08-21 11:34:03 +0300

    feat: add markerChange event emitter

--- src/app/stations/components/stations-page/stations-page.component.ts
+++ src/app/stations/components/stations-page/stations-page.component.ts
@@ -12,4 +12,8 @@ import { MapComponent } from '../map/map.component';
   styleUrl: './stations-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StationsPageComponent {}
+export class StationsPageComponent {
+  public onMarkerChange(event: unknown) {
+    console.log(event);
+  }
+}
