487a42e6dc33695ed4cc9077337e0c0989963e0d
VKTRenokh <viktorenokh@gmail.com>
2024-08-21 15:21:16 +0300

    feat: add city input

--- src/app/stations/components/stations-page/stations-page.component.ts
+++ src/app/stations/components/stations-page/stations-page.component.ts
@@ -3,17 +3,29 @@ import {
   Component,
   inject,
 } from '@angular/core';
-import { MapComponent } from '../map/map.component';
+import {
+  defaultLatLng,
+  MapComponent,
+} from '../map/map.component';
 import {
   NonNullableFormBuilder,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
 import { AsyncPipe } from '@angular/common';
+import { TuiButton } from '@taiga-ui/core';
+import { TuiInputModule } from '@taiga-ui/legacy';
 
 @Component({
   selector: 'tra-stations-page',
   standalone: true,
-  imports: [MapComponent, ReactiveFormsModule, AsyncPipe],
+  imports: [
+    MapComponent,
+    ReactiveFormsModule,
+    AsyncPipe,
+    TuiButton,
+    TuiInputModule,
+  ],
   templateUrl: './stations-page.component.html',
   styleUrl: './stations-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,12 +34,19 @@ export class StationsPageComponent {
   private formBuilder = inject(NonNullableFormBuilder);
 
   public form = this.formBuilder.group({
-    latLng: this.formBuilder.control(null),
+    latLng: this.formBuilder.control(defaultLatLng),
+    city: this.formBuilder.control('', [
+      Validators.required,
+    ]),
   });
 
   constructor() {
     this.form.controls.latLng.valueChanges.subscribe(
       console.log,
     );
+  }
+
+  public onSubmit() {
+    console.dir(this.form.getRawValue(), { depth: null });
   }
 }
