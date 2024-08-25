5623f09602f9e315b4004293b2ec6406f862cb8a
VKTRenokh <viktorenokh@gmail.com>
2024-08-25 17:33:10 +0300

    feat: implement correct pipe logic

--- src/app/stations/pipes/connected-names.pipe.ts
+++ src/app/stations/pipes/connected-names.pipe.ts
@@ -1,4 +1,9 @@
-import { inject, Pipe, PipeTransform } from '@angular/core';
+import {
+  computed,
+  inject,
+  Pipe,
+  PipeTransform,
+} from '@angular/core';
 import { Station } from '../models/station/station.model';
 import { StationsFacade } from '../../state/facades/stations.facade';
 
@@ -12,16 +17,23 @@ export class ConnectedNamesPipe implements PipeTransform {
 
   private stations = this.stationsFacade.stations;
 
-  private idToCityMap = new Map(
-    this.stations()?.map(station => [
-      station.id,
-      station.city,
-    ]),
-  );
+  private idToCityMap = computed(() => {
+    const stations = this.stations();
+    return new Map(
+      stations
+        ? stations.map(station => [
+            station.id,
+            station.city,
+          ])
+        : [],
+    );
+  });
 
   public transform(currentStation: Station): string {
+    const map = this.idToCityMap();
+
     const connectedNames = currentStation.connectedTo
-      .map(station => this.idToCityMap.get(station.id))
+      .map(station => map.get(station.id))
       .filter(Boolean);
 
     return connectedNames.join(', ');
