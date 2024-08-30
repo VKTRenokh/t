9abc6efcfe38bc3024967a9012a8e4626de3df24
VKTRenokh <viktorenokh@gmail.com>
2024-08-30 12:14:42 +0300

    feat: add routes actions handlers

--- src/app/state/reducers/routes.reducer.ts
+++ src/app/state/reducers/routes.reducer.ts
@@ -1,6 +1,7 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
 import { Route } from '../../routes/models/route/route.model';
 import { ApiError } from '../../shared/models/api-error.model';
+import { RoutesActions } from '../actions/routes.action';
 
 export interface RoutesState {
   error?: ApiError;
@@ -13,4 +14,23 @@ export const initialState: RoutesState = {
   routes: [],
 };
 
-export const routesReducer = createReducer(initialState);
+export const routesReducer = createReducer(
+  initialState,
+  on(RoutesActions.getRoutes, state => ({
+    ...state,
+    loading: true,
+  })),
+  on(
+    RoutesActions.getRoutesSuccess,
+    (state, { routes }) => ({
+      ...state,
+      routes,
+      loading: false,
+    }),
+  ),
+  on(RoutesActions.failure, (state, { error }) => ({
+    ...state,
+    loading: false,
+    error,
+  })),
+);
