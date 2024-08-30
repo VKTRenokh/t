cf12f176f634041bfd7195d1db093ceb3725d420
VKTRenokh <viktorenokh@gmail.com>
2024-08-30 17:09:16 +0300

    feat: implement filter-by-connection pipe

--- src/app/routes/pipes/filter-by-connection/filter-by-connection.pipe.ts
+++ src/app/routes/pipes/filter-by-connection/filter-by-connection.pipe.ts
@@ -10,8 +10,31 @@ export class FilterByConnectionPipe
 {
   public transform(
     ids: number[],
+    selectedIds: number[],
     stations: Station[],
   ): number[] {
-    return ids;
+    if (selectedIds.length === 0) {
+      return ids;
+    }
+
+    const lastSelectedId =
+      selectedIds[selectedIds.length - 1];
+
+    const lastSelectedStation = stations.find(
+      station => station.id === lastSelectedId,
+    );
+
+    if (!lastSelectedStation) {
+      return [];
+    }
+
+    const connectedStationIds =
+      lastSelectedStation.connectedTo.map(
+        connection => connection.id,
+      );
+
+    return ids.filter(id =>
+      connectedStationIds.includes(id),
+    );
   }
 }
