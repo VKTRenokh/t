5d170c96d411bcb3ddc88d3cf64e00d325ea791f
VKTRenokh <viktorenokh@gmail.com>
2024-09-02 17:21:35 +0300

    feat: add available-ride component

---
+++ src/app/search/components/available-ride/available-ride.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'tra-available-ride',
+  standalone: true,
+  imports: [],
+  templateUrl: './available-ride.component.html',
+  styleUrl: './available-ride.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AvailableRideComponent {
+
+}
