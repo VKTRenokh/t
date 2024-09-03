1b3906e81f3ea3aa9797ef72d1b631d9c68d44cd
ivanFihu <verbipo@gmail.com>
2024-09-03 23:32:09 +0200

    feat: add final time

--- src/app/orders/components/orders-page/orders-page.component.ts
+++ src/app/orders/components/orders-page/orders-page.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import {
   ChangeDetectionStrategy,
   Component,
@@ -7,12 +6,12 @@ import {
 import { mockOrderResponse } from '../../models/mock-response';
 import { ProfileFacade } from '../../../profile/services/profile-facade.service';
 import { Order } from '../../models/orders';
-import { DatePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'tra-orders-page',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, CurrencyPipe],
   templateUrl: './orders-page.component.html',
   styleUrl: './orders-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,20 +21,60 @@ export class OrdersPageComponent {
 
   public profile = this.profileFacade.profile;
 
-  private http = inject(HttpClient);
   public mockOrder = mockOrderResponse;
 
-  public getDepartureTime(journeyData: Order) {
-    const { path, stationStart, schedule } = journeyData;
-    const startIndex = path.indexOf(stationStart);
-    return new Date(schedule.segments[startIndex].time[0]);
-  }
+  public calculateJourneyDetails(journeyData: Order) {
+    const {
+      path,
+      stationStart,
+      stationEnd,
+      carriages,
+      schedule,
+    } = journeyData;
 
-  public getArrivalsTime(journeyData: Order) {
-    const { path, stationEnd, schedule } = journeyData;
+    const startIndex = path.indexOf(stationStart);
     const endIndex = path.indexOf(stationEnd);
-    return new Date(
+
+    if (startIndex === -1 || endIndex === -1) {
+      throw new Error(
+        'Start or end station not found in the path',
+      );
+    }
+
+    const departureTime = new Date(
+      schedule.segments[startIndex].time[0],
+    );
+    const arrivalTime = new Date(
       schedule.segments[endIndex - 1].time[1],
     );
+
+    const totalTimeMs =
+      arrivalTime.getTime() - departureTime.getTime();
+
+    const carriageType = carriages[0]; // need to update
+    let totalPrice = 0;
+
+    for (let i = startIndex; i < endIndex; i++) {
+      totalPrice +=
+        schedule.segments[i].price[carriageType] || 0;
+    }
+
+    return {
+      departureTime,
+      arrivalTime,
+      totalTimeMs,
+      totalPrice,
+    };
+  }
+  //move to utils
+  public formatDuration(ms: number): string {
+    const seconds = Math.floor(ms / 1000);
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    return [hours, minutes, remainingSeconds]
+      .map(v => v.toString().padStart(2, '0'))
+      .join(':');
   }
 }
